b0062615840fa232304a2b33775e9157d604c221
Patrick Buller <pbuller212@gmail.com>
2025-06-26 07:17:01 -0500

    Failed to get basic hello world working.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai.llms import OpenAI
+
+load_dotenv()
+model = OpenAI(model="text-davinci-003", temperature=0, api_key=os.getenv('OPENAI_API_KEY'))
+
 def main():
     print("Hello from learning-langchain-notes!")
-
+    model.invoke("The sky is")
 
 if __name__ == "__main__":
     main()
